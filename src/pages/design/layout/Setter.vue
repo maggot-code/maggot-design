6a45ae80baa4db6b52c50ad7adbfbc21fc2de1ef
maggot_code <wowhuanhuan233@163.com>
2022-10-14 15:43:07 +0800

    fix :-1:

--- src/pages/design/layout/Setter.vue
+++ src/pages/design/layout/Setter.vue
@@ -3,27 +3,45 @@
  * @Author: maggot-code
  * @Date: 2022-10-13 09:49:30
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-10-14 11:01:36
+ * @LastEditTime: 2022-10-14 11:33:57
  * @Description: 
 -->
 <script setup>
+import SetterBasic from "../layout/SetterBasic.vue";
+import SetterHigh from "../layout/SetterHigh.vue";
+import SetterDrive from "../layout/SetterDrive.vue";
+
+import { ref } from "vue";
 import { useActive } from "../hook/useActive";
-import { useMatterSelect } from "../hook/useMatterSelect";
 
+const setterName = ref("basic");
 const { target } = useActive();
-const select = useMatterSelect();
-
-console.log(select);
-
 </script>
 
 <template>
-    <div>
-        <h1>选中控件的属性设置</h1>
-        <p>{{target}}</p>
-    </div>
+    <el-tabs class="design-setter" v-model="setterName" :stretch="true">
+        <el-tab-pane label="基础属性" name="basic">
+            <SetterBasic class="design-setter-layout"></SetterBasic>
+        </el-tab-pane>
+
+        <el-tab-pane label="高级属性" name="high">
+            <SetterHigh class="design-setter-layout"></SetterHigh>
+        </el-tab-pane>
+
+        <el-tab-pane label="联动属性" name="drive">
+            <SetterDrive class="design-setter-layout"></SetterDrive>
+        </el-tab-pane>
+    </el-tabs>
 </template>
 
 <style scoped lang='scss'>
+.design-setter {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
 
+    & :deep(.el-tabs__content) {
+        height: calc(100% - 55px);
+    }
+}
 </style>
---
+++ src/pages/design/layout/SetterBasic.vue
@@ -0,0 +1,19 @@
+<!--
+ * @FilePath: \maggot-design\src\pages\design\layout\SetterBasic.vue
+ * @Author: maggot-code
+ * @Date: 2022-10-14 11:16:38
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-10-14 11:23:16
+ * @Description: 
+-->
+<script setup></script>
+
+<template>
+    <section class="design-setter-basic">
+        基础属性
+    </section>
+</template>
+
+<style scoped lang='scss'>
+
+</style>
